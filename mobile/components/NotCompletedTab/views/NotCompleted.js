fd11e4d16e8404d6ffff1792f4ee1f68a59cfe09
Jordan Daniels <decentpianist@gmail.com>
2018-03-31 13:58:10 -0700

    added spinning loading icons

--- mobile/components/NotCompletedTab/views/NotCompleted.js
+++ mobile/components/NotCompletedTab/views/NotCompleted.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'red'
   },
   title:{
     flexDirection: 'column',
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
   },
   spinner:{
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   }
 });
 
@@ -57,8 +57,8 @@ class NotCompleted extends Component {
   render() {
       return (
         <View style={styles.spinnerContainer}>
-          <Text> Not Completed </Text>
           <Spinner type='FadingCircle' style={styles.spinner}/>
+          <Text> Not Completed </Text>
         </View>
       )
     }
