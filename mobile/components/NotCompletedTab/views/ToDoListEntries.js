6bd39fbe5af8640bdb762bab0123903a270a9d5d
Jordan Daniels <decentpianist@gmail.com>
2018-04-02 11:38:01 -0700

    changed styling for list items

--- mobile/components/NotCompletedTab/views/ToDoListEntries.js
+++ mobile/components/NotCompletedTab/views/ToDoListEntries.js
@@ -39,19 +39,21 @@ class ToDoList extends Component {
     return (
       <View>
         <Swipeout right={swipeoutBtns} style={styles.toDoEntry}>
-          <View style={(styles.padding, styles.border)}>
+          <View style={(styles.padding, styles.border, styles.columns)}>
             <TouchableOpacity
               onPress={() => this.changeStatus(todo, "completed")}
               style={styles.red}
             >
-              <Image 
-                source={require('../../../TabPhotos/notComplete.png')}
-                style={{width:20, height:20}}>
-              </Image>
+              <Image
+                source={require("../../../TabPhotos/notComplete.png")}
+                style={styles.image}
+              />
             </TouchableOpacity>
             <Text style={styles.toDoEntry}>
-              {todo.content} {new Date(parseInt(todo.timeStamp)).toUTCString()}
-              <Text style={styles.status}>{todo.status}</Text>
+              {todo.content} {"\n"}
+              <Text style={styles.time}>
+                {new Date(parseInt(todo.timeStamp)).toUTCString()}
+              </Text>
             </Text>
           </View>
         </Swipeout>
@@ -63,11 +65,14 @@ class ToDoList extends Component {
 const styles = StyleSheet.create({
   toDoEntry: {
     height: 80,
-    backgroundColor: "white"
+    backgroundColor: "white",
+    padding: 14,
+    margin: 1,
+    fontSize: 16
   },
-  status: {
-    fontSize: 18,
-    color: "red",
+  time: {
+    fontSize: 10,
+    color: "green",
     alignItems: "flex-end"
   },
   padding: {
@@ -77,6 +82,17 @@ const styles = StyleSheet.create({
     borderRadius: 2,
     borderWidth: 0.5,
     borderColor: "gray"
+  },
+  columns: {
+    flexDirection: "row",
+    alignItems: "center",
+    textAlign: "center"
+  },
+  image: {
+    width: 20,
+    height: 20,
+    margin: 3,
+    bottom: 10
   }
 });
 
